5671111370aa3f2fcf4ff2882ae6c9876aca37df
kobalt42z <chmouel2690@gmail.com>
2023-01-05 18:58:39 +0000

    new

--- test.c
+++ test.c
@@ -1,1 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct point
+{
+    int x;
+    int y;
+} point;
+
+typedef struct polygon
+{
+    int amount;
+    point **points[];
+} polygon;
+
+int main(int argc, char const *argv[])
+{
+    point a = {5, 3};
+    point b = {7, 9};
+    point c = {6, 9};
+
+    return 0;
+}
